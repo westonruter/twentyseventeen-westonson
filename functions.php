4bc5437aa0b358afa1492cea40d580e73fff7cb6
Weston Ruter <weston@xwp.co>
2017-11-14 19:56:00 -0800

    Add footer site info as setting

---
+++ functions.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Theme functions.
+ *
+ * @package Twenty_Seventeen_Westonson
+ */
+
+define( 'TWENTYSEVENTEEN_WESTONSON_DEFAULT_FOOTER_SITE_INFO', sprintf(
+	'<a href="%s">%s</a>',
+	esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ),
+	/* translators: placeholder is WordPress */
+	sprintf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'WordPress' )
+) );
+
+add_action( 'customize_register', function( WP_Customize_Manager $wp_customize ) {
+	$wp_customize->add_setting( 'footer_site_info', array(
+		'type' => 'option',
+		'transport' => 'postMessage',
+		'default' => TWENTYSEVENTEEN_WESTONSON_DEFAULT_FOOTER_SITE_INFO,
+		'sanitize_callback' => function( $value ) {
+			return wp_kses_post( $value );
+		},
+	) );
+
+	$wp_customize->add_control( new WP_Customize_Code_Editor_Control( $wp_customize, 'footer_site_info', array(
+		'label' => 'Footer',
+		'section' => 'title_tagline',
+		'code_type' => 'text/html',
+		'settings' => array( 'footer_site_info' ),
+		'priority' => 45,
+	) ) );
+
+	$wp_customize->selective_refresh->add_partial( 'footer_site_info', array(
+		'settings' => array( 'footer_site_info' ),
+		'selector' => '.site-info',
+		'container_inclusive' => true,
+		'render_callback' => function() {
+			\get_template_part( 'template-parts/footer/site', 'info' );
+		},
+	) );
+} );
---
+++ template-parts/footer/site-info.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package Twenty_Seventeen_Westonson
+ */
+
+?>
+<div class="site-info">
+	<?php echo get_option( 'footer_site_info', TWENTYSEVENTEEN_WESTONSON_DEFAULT_FOOTER_SITE_INFO ); ?>
+</div><!-- .site-info -->
