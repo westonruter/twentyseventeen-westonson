ac13a9e21c500b02686c618c3b23648895f3318d
Weston Ruter <weston@xwp.co>
2018-08-21 11:21:06 -0700

    Fix formatting for phpcs

--- functions.php
+++ functions.php
@@ -14,27 +14,27 @@ define( 'TWENTYSEVENTEEN_WESTONSON_DEFAULT_FOOTER_SITE_INFO', sprintf(
 
 add_action( 'customize_register', function( WP_Customize_Manager $wp_customize ) {
 	$wp_customize->add_setting( 'footer_site_info', array(
-		'type' => 'option',
-		'transport' => 'postMessage',
-		'default' => TWENTYSEVENTEEN_WESTONSON_DEFAULT_FOOTER_SITE_INFO,
-		'sanitize_callback' => function( $value ) {
+		'type'              => 'option',
+		'transport'         => 'postMessage',
+		'default'           => TWENTYSEVENTEEN_WESTONSON_DEFAULT_FOOTER_SITE_INFO,
+		'sanitize_callback' => function ( $value ) {
 			return wp_kses_post( $value );
 		},
 	) );
 
 	$wp_customize->add_control( new WP_Customize_Code_Editor_Control( $wp_customize, 'footer_site_info', array(
-		'label' => 'Footer',
-		'section' => 'title_tagline',
+		'label'     => 'Footer',
+		'section'   => 'title_tagline',
 		'code_type' => 'text/html',
-		'settings' => array( 'footer_site_info' ),
-		'priority' => 45,
+		'settings'  => array( 'footer_site_info' ),
+		'priority'  => 45,
 	) ) );
 
 	$wp_customize->selective_refresh->add_partial( 'footer_site_info', array(
-		'settings' => array( 'footer_site_info' ),
-		'selector' => '.site-info',
+		'settings'            => array( 'footer_site_info' ),
+		'selector'            => '.site-info',
 		'container_inclusive' => true,
-		'render_callback' => function() {
+		'render_callback'     => function () {
 			\get_template_part( 'template-parts/footer/site', 'info' );
 		},
 	) );
