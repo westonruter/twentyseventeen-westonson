b84a36f6f6c6faa590c87580bcc7e0aeb3ca3e4f
Weston Ruter <westonruter@google.com>
2018-10-14 18:28:09 -0700

    Remove obsolete styling for loading indicator

--- functions.php
+++ functions.php
@@ -11,63 +11,6 @@ add_theme_support( 'amp', array(
 
 add_theme_support( 'service_worker_streaming' );
 
-add_action( 'wp_print_styles', function() {
-	if ( ! class_exists( 'WP_Service_Worker_Navigation_Routing_Component' ) || 'header' !== get_query_var( WP_Service_Worker_Navigation_Routing_Component::STREAM_FRAGMENT_QUERY_VAR ) ) {
-		return;
-	}
-
-	?>
-	<style>
-		#stream-loading-message {
-			position:fixed;
-			text-align: center;
-			font-style: italic;
-			bottom: 0;
-			left: 0;
-			right: 0;
-			background-color: rgba( 255, 255, 255, 0.5 );
-		}
-
-		.lds-ring {
-			display: inline-block;
-			position: relative;
-			width: 64px;
-			height: 64px;
-		}
-		.lds-ring div {
-			box-sizing: border-box;
-			display: block;
-			position: absolute;
-			width: 51px;
-			height: 51px;
-			margin: 6px;
-			border: 6px solid #000;
-			border-radius: 50%;
-			animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-			border-color: #000 transparent transparent transparent;
-		}
-		.lds-ring div:nth-child(1) {
-			animation-delay: -0.45s;
-		}
-		.lds-ring div:nth-child(2) {
-			animation-delay: -0.3s;
-		}
-		.lds-ring div:nth-child(3) {
-			animation-delay: -0.15s;
-		}
-		@keyframes lds-ring {
-			0% {
-				transform: rotate(0deg);
-			}
-			100% {
-				transform: rotate(360deg);
-			}
-		}
-
-	</style>
-	<?php
-} );
-
 // Make sure the precached streaming header varies by the header logo and header image.
 add_filter( 'wp_streaming_header_precache_entry', function( $entry ) {
 	if ( isset( $entry['revision'] ) ) {
