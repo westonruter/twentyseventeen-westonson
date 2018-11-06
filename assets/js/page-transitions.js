97977df4f2ea0906d4c0ded51c9bdcdce142a553
Piotr Delawski <piotr.delawski@xwp.co>
2018-11-06 21:12:02 +0100

    AC-5 - Use AMP WP events in page transitions event handler.

--- assets/js/page-transitions.js
+++ assets/js/page-transitions.js
@@ -8,43 +8,28 @@ window.addEventListener( 'wp-amp-app-shell-ready', ( event ) => {
 	console.info( event.type, event.detail );
 } );
 
-window.addEventListener( 'click', function( e ) {
-	var node = e.target;
+window.addEventListener( 'wp-amp-app-shell-navigate', function( event ) {
 	var homeUrl = window.wpPageTransitions.homeUrl;
+	var currentUrl = event.detail.url.href;
 
-	while ( null !== node ) {
+	// If the link has been already clicked, prevent subsequent clicks.
+	if ( document.body.classList.contains( 'page-transition' ) ) {
+		return;
+	}
 
-		// Search for any external links.
-		if ( node.href && 0 !== node.href.replace( document.location.href, '' ).indexOf( '#' ) ) {
+	// Remove trailing slash.
+	if ( currentUrl.lastIndexOf( '/' ) === currentUrl.length - 1 ) {
+		currentUrl = currentUrl.substr( 0, currentUrl.length - 1 );
+	}
 
-			// If the link has been already clicked, prevent subsequent clicks.
-			if ( document.body.classList.contains( 'page-transition' ) ) {
-				e.preventDefault();
-				e.stopPropagation();
-				return false;
-			}
+	// Scroll back to top if redirecting to a template with a different header height.
+	if ( currentUrl === homeUrl || document.body.classList.contains( 'twentyseventeen-front-page' ) ) {
+		window.scrollTo( 0, 0 );
+	}
 
-			var href = node.href;
-
-			// Remove trailing slash.
-			if ( href.lastIndexOf( '/' ) === href.length - 1 ) {
-				href = href.substr( 0, href.length - 1 );
-			}
-
-			// Scroll back to top if redirecting to a template with a different header height.
-			if ( href === homeUrl || document.body.classList.contains( 'twentyseventeen-front-page' ) ) {
-				window.scrollTo( 0, 0 );
-			}
-
-			// Add transition related classes to the BODY tag.
-			document.body.classList.add( 'page-transition' );
-			if ( href === homeUrl ) {
-				document.body.classList.add( 'home-page-transition' );
-			}
-
-			return;
-		}
-
-		node = node.parentNode;
+	// Add transition related classes to the BODY tag.
+	document.body.classList.add( 'page-transition' );
+	if ( currentUrl === homeUrl ) {
+		document.body.classList.add( 'home-page-transition' );
 	}
 });
